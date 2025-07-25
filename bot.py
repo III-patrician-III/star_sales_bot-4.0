838c5f52869884dc1d2d80b55c61686b2ce3db8d
III-patrician-III <qpalzm11324@gmail.com>
2025-07-25 11:53:33 +0300

    Remove unsed files

--- bot.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import os
-
-bot = Bot(token=os.getenv("BOT_TOKEN"))
-dp = Dispatcher(bot, storage=MemoryStorage())
